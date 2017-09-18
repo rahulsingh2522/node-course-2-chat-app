1cd258312d14c89ce2e30410323eb5bd72eacc78
rahul.singh@salesforce.com <Rahul Singh>
2017-09-17 23:43:50 -0400

    Add newMessage and createMessage

--- server/server.js
+++ server/server.js
@@ -15,6 +15,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New User Connected');
 
+    socket.emit('newMessage', {
+        from: 'mike@example.com',
+        text: 'Can you join at 6 PM',
+        createAt: 123
+    });
+
+    socket.on('createMessage', (newMessage) => {
+        console.log('createMessage', newMessage);
+    });
+
     socket.on('disconnect', () => {
             console.log('User was disconnected');
         });
