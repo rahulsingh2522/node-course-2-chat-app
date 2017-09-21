8822a6e234a5cbac3d6007df70b651404a4c624a
rahul.singh@salesforce.com <Rahul Singh>
2017-09-20 22:58:59 -0400

    Add form for message and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function ()  {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'jen@example.com',
-        text: 'Yes. I can join..'
-    });
 });
 
 socket.on('disconnect', function () {
@@ -15,4 +10,19 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     console.log('New message', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault();
+    console.log('In jQuery..');
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function () {
+
+    });
 });
--- server/server.js
+++ server/server.js
@@ -20,19 +20,14 @@ io.on('connection', (socket) => {
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-     socket.on('createMessage', (newMessage) => {
+     socket.on('createMessage', (newMessage, callback) => {
         console.log('createMessage', newMessage);
         io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
 
-        // socket.broadcast.emit('newMessage', {
-        //     from: newMessage.from,
-        //     text: newMessage.text,
-        //     createdAt: new Date().getTime()
-        // });
-
-     });
-
-    
+       if(callback) {
+            callback('This is from the server.');
+       }     
+     });    
 
     socket.on('disconnect', () => {
             console.log('User was disconnected');
